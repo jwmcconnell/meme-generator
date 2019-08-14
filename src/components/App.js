60d4f132d31f666ffc1a60944b3c32edd00923d3
Jackmc <jackmc1122@gmail.com>
2019-08-14 13:30:46 -0700

    Make App a class component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
   
+export default App;
